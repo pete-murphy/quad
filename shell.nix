441b7bb148caddffd3a5db5fbb8940a06317a2a9
Peter Murphy <pete@simspace.com>
2022-04-13 19:29:14 -0400

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,5 +1,8 @@
-# https://input-output-hk.github.io/haskell.nix/tutorials/development.html#how-to-get-a-development-shell
-let
-  project = import ./default.nix;
-in
-  project.shellFor {}
+# https://input-output-hk.github.io/haskell.nix/tutorials/getting-started.html#working-with-a-project
+(import ./default.nix).shellFor {
+  tools = {
+    cabal = "3.2.0.0";
+    hlint = "latest";
+    haskell-language-server = "latest";
+  };
+}
