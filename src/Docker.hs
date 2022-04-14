a214a157b971ed88adb278d3abfd7d8c347ff264
Peter Murphy <pete@simspace.com>
2022-04-13 23:43:48 -0400

    p 31 (add startContainer)

--- src/Docker.hs
+++ src/Docker.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeApplications #-}
 {-# OPTIONS_GHC -Wno-deprecations #-}
 {-# OPTIONS_GHC -option -pgmF=record-dot-preprocessor #-}
 
 module Docker where
 
+import Data.Aeson ((.:))
 import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Types as Aeson.Types
 import qualified Network.HTTP.Simple as HTTP
 import RIO
 import qualified Socket
@@ -12,21 +16,60 @@ newtype CreateContainerOptions
   = CreateContainerOptions
       {image :: Image}
 
-createContainer :: CreateContainerOptions -> IO ()
+newtype ContainerId = ContainerId Text
+  deriving (Eq, Show)
+
+containerIdToText :: ContainerId -> Text
+containerIdToText (ContainerId c) = c
+
+createContainer :: CreateContainerOptions -> IO ContainerId
 createContainer options = do
   manager <- Socket.newManager "/var/run/docker.sock"
   let image = imageToText options.image
   let body =
         Aeson.object
-          [("Image", Aeson.toJSON image)]
+          [ ("Image", Aeson.toJSON image),
+            ("Tty", Aeson.toJSON True),
+            ("Labels", Aeson.object [("quad", "")]),
+            ("Cmd", "echo hello"),
+            ("Entrypoint", Aeson.toJSON [Aeson.String "/bin/sh", "-c"])
+            -- ("Entrypoint", Aeson.toJSON (["/bin/sh", "-c"] :: [Aeson.Value]))
+          ]
   let req =
         HTTP.defaultRequest
           & HTTP.setRequestManager manager
           & HTTP.setRequestPath "/v1.40/containers/create"
           & HTTP.setRequestMethod "POST"
           & HTTP.setRequestBodyJSON body
+  let parser = Aeson.withObject "create-container" \o -> do
+        cId <- o .: "Id"
+        pure (ContainerId cId)
   res <- HTTP.httpBS req
-  traceShowIO res
+  parseResponse res parser
+
+parseResponse ::
+  HTTP.Response ByteString ->
+  (Aeson.Value -> Aeson.Types.Parser a) ->
+  IO a
+parseResponse res parser = do
+  let result = do
+        value <- Aeson.eitherDecodeStrict (HTTP.getResponseBody res)
+        Aeson.Types.parseEither parser value
+  case result of
+    Left e -> throwString e
+    Right status -> pure status
+
+startContainer :: ContainerId -> IO ()
+startContainer container = do
+  manager <- Socket.newManager "/var/run/docker.sock"
+  let path =
+        "/v1.40/containers/" <> containerIdToText container <> "/start"
+  let req =
+        HTTP.defaultRequest
+          & HTTP.setRequestManager manager
+          & HTTP.setRequestPath (encodeUtf8 path)
+          & HTTP.setRequestMethod "POST"
+  void (HTTP.httpBS req)
 
 newtype Image = Image Text
   deriving (Eq, Show)
